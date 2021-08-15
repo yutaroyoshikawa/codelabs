27c5d78c716392c8577acb54819de9e42e210e3e
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2021-08-15 22:27:53 +0900

    fix: claat command path

--- scripts/export_codelabs.sh
+++ scripts/export_codelabs.sh
@@ -4,4 +4,5 @@ BUILD_DIR=build_codelabs
 
 claat export -o $BUILD_DIR codelabs/shallow-equal-vs-deep-equal.md
 claat export -o $BUILD_DIR codelabs/react-component-key.md
-claat export -o $BUILD_DIR truncate-css-as-grid-layout.md
+claat export -o $BUILD_DIR codelabs/truncate-css-as-grid-layout.md
+claat export -o $BUILD_DIR codelabs/logic-of-virtual-scroll.md
